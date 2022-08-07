b8feb54d8ae7db8ee0777d3efb21b080af549970
cristian290598 <cristiandgc@gmail.com>
2022-08-06 22:19:07 -0500

    createproyect

--- src/app/company-data/company-data.module.ts
+++ src/app/company-data/company-data.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { CompanyDataRoutingModule } from './company-data-routing.module';
 import { CreateCompanyComponent } from './create-company/create-company.component';
 import { CreateMethodologyComponent } from './create-methodology/create-methodology.component';
+import { CreateProyectComponent } from './create-proyect/create-proyect.component';
 
 
 @NgModule({
   declarations: [
     CreateCompanyComponent,
-    CreateMethodologyComponent
+    CreateMethodologyComponent,
+    CreateProyectComponent
   ],
   imports: [
     CommonModule,
